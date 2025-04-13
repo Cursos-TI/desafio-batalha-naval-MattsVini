7aec7d648dad48dbc2f331d3e3f132089542923e
MattsVini <151637752+MattsVini@users.noreply.github.com>
2025-04-13 12:38:31 +0000

    Criação da tabela e dos vetores e matrizes

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,57 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int tabuleiro[10][10];
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    char y = 'A';
+    int x = 1;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    int navioVertical[3] = {3, 3, 3};
+    int navioHorizontal[3] = {3, 3, 3};
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    //Colocando os zeros na matriz.
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
 
-    return 0;
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    //colocando os navios no tabuleiro por meio do loop for com múltiplas variáveis.
+    for(int i = 0, j = 5; i < 3; i++, j++){
+        tabuleiro[1][j] = navioHorizontal[i];
+    }
+
+    for(int i = 0, j = 6; i < 3; i++, j++){
+        tabuleiro[j][2] = navioHorizontal[i];
+    }
+    //Espaçamento para organizar a saída de dados.
+    printf("    ");
+
+
+    //Exibir o tabuleiro.
+    //coordenada y
+    for(int i = 0; i < 10; i++){
+        printf("%c ", y);
+        y++;
+    }
+    printf("\n");
+    for(int i = 0; i < 10; i++){
+
+    //coordenada x    
+    if(x == 10){
+         printf(" %d ", x);
+    } else{
+     printf("  %d ", x);
+     x++;
+}
+
+    for(int j = 0; j < 10; j++){
+              
+    printf("%d ",tabuleiro[i][j]);
+        
+    }
+    printf("\n");
+ }
 }
